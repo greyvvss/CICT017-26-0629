90bc14f4ff9e09b0ace2a6bbb43b102238b968d1
greyvvss <phillipantony81@gmail.com>
2026-09-09 09:39:22 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+function darkMode() {
+    document.body.style.backgroundColor = "black";
+    document.body.style.color = "white";
+}
+
+
+function lightMode() {
+    document.body.style.backgroundColor = "white";
+    document.body.style.color = "black";
+}
